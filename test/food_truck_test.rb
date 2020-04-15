03cdd9f6229f80124e90a815da825bc7edb9fe53
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-15 10:09:54 -0600

    Pass test_it_can_check_stock

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -7,6 +7,8 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
   def setup
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_exists
@@ -16,5 +18,9 @@ class FoodTruckTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Rocky Mountain Pies", @food_truck.name
     assert_equal ({}), @food_truck.inventory
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
   end
 end
