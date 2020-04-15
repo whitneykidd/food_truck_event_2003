0f749d85be3f49e2930717cd2bb57a46999ac35d
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-15 10:05:02 -0600

    Pass test_it_has_attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,5 +1,8 @@
 class Item
-  def initialize(name)
-    @name = name
+  attr_reader :name, :price
+  def initialize(attributes)
+    @name = attributes[:name]
+    @price = attributes[:price][1..-1].to_f.to_f
+
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -5,10 +5,15 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
   def setup
-    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_exists
-    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
+  end
+
+  def test_it_has_attributes
+    assert_equal "Apple Pie (Slice)", @item2.name
+    assert_equal 2.50, @item2.price
   end
 end
