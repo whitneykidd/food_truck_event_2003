c75b8acf1e5ced500e87d57d42b47bbad2239e27
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-15 10:08:05 -0600

    Pass test_it_has_attributes

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,5 +1,7 @@
 class FoodTruck
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
+    @inventory = {}
   end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -12,4 +12,9 @@ class FoodTruckTest < Minitest::Test
   def test_it_exists
     assert_instance_of FoodTruck, @food_truck
   end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal ({}), @food_truck.inventory
+  end
 end
